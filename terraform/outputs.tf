f1ed96eb35d547808bf6c008090246caa176d26e
Marc Huffnagle <marc@element84.com>
2019-07-02 10:24:57 -0400

    WIP Handing off to Jacob

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,17 @@ output "api_endpoint" {
   value = var.domain_name == null ? aws_cloudformation_stack.thin_egress_app.outputs.ApiEndpoint : "https://${var.domain_name}/"
 }
 
+output "rest_api" {
+  value = {
+    id               = aws_cloudformation_stack.thin_egress_app.outputs.RestApiId,
+    root_resource_id = aws_cloudformation_stack.thin_egress_app.outputs.RestApiRootResourceId
+  }
+}
+
+output "rest_api_stage_name" {
+  value = aws_cloudformation_stack.thin_egress_app.outputs.RestApiDeploymentStage
+}
+
 output "urs_redirect_uri" {
   value = var.domain_name == null ? aws_cloudformation_stack.thin_egress_app.outputs.URSredirectURI : "https://${var.domain_name}/login"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -109,6 +109,12 @@ variable "stage_name" {
   description = "This value will show up as the 'base' of the url path as so: https://xxxxxxxx.execute-api.us-east-1.amazonaws.com/<StageName>/and/so/on."
 }
 
+variable "template_body" {
+  type        = string
+  default     = null
+  description = "A CloudFormation template"
+}
+
 variable "template_url" {
   type        = string
   default     = "https://s3.amazonaws.com/asf.public.code/thin-egress-app/tea-cloudformation-<BUILD_ID>.yaml"
